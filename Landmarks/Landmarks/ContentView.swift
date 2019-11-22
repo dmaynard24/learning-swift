8983e49869cd91f64d68bc402129484868f8fcd5
Dave Maynard <davemaynard24@gmail.com>
2019-11-21 21:08:57 -0500

    created new Landmarks Xcode project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dave Maynard on 11/21/19.
+//  Copyright © 2019 Dave Maynard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
