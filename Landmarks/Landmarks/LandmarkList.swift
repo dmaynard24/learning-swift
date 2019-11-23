59626c1ff9cda558c5c405a7944fcfc8e586988c
Dave Maynard <davemaynard24@gmail.com>
2019-11-23 14:49:08 -0500

    started favorites functionality, added a toggle to filter the list by favorites

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -9,11 +9,21 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = true
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
@@ -23,10 +33,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
