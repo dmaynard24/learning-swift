e993024b845ef12d35c9cc6b27f58ea23a254acd
Dave Maynard <davemaynard24@gmail.com>
2019-11-24 19:50:53 -0500

    started working with Paths, creating the app icon

---
+++ Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Dave Maynard on 11/24/19.
+//  Copyright © 2019 Dave Maynard. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
